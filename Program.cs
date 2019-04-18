4ad6a5237069a357d175c32f843cd143d4f6d38d
Andre Baltieri <andrebaltieri@hotmail.com>
2019-04-18 20:27:52 -0300

    Exemplo inicial

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+
+namespace efseis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class ProductMap : EntityTypeConfiguration<Product>
+    {
+        public ProductMap()
+        {
+            ToTable("Product");
+            HasKey(x => x.Id);
+            Property(x => x.Title).IsRequired();
+        }
+    }
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("CONNSTR") { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProductMap());
+        }
+    }
+}
